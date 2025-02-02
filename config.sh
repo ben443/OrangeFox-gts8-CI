9080b5a3c32a4a3e7994d0b3f1062b32e3274ccd
GrenJikxd <124490546+GrenJIkxd@users.noreply.github.com>
2025-02-02 08:10:11 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/clhexareslab/recovery_device_xiaomi_ares.git -b fox_11.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/GrenJIkxd/android_device_samsung_gts8.git -b main"
 
-export DEVICE="ares"
-export OEM="xiaomi"
+export DEVICE="gts8"
+export OEM="samsung"
 export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-#export KERNEL_SOURCE="https://gitlab.com/OrangeFox/kernel/mojito.git"
-#export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
+export KERNEL_SOURCE="https://github.com/GrenJIkxd/kernel_samsung_sm8450.git"
+export PLATFORM="SM-X700" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
-export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
+#export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
